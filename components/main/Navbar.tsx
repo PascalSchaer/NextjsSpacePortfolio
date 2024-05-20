7fd0b9714e836cb82e9df2bcdde3f0c9f322db1c
Pascal <pascalschaer8@gmail.com>
2024-05-20 10:09:39 -0600

    change to navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="w-full h-full flex flex-row items-center justify-center m-auto px-[10px]">
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#hero" className="cursor-pointer">
+            <a href="#" className="cursor-pointer">
               About me
             </a>
             <a href="#skills" className="cursor-pointer">
